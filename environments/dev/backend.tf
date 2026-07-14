da810f1a2e80d9761827405cb7ffee894c066f04
sreemural <sreelajavm@gmail.com>
2026-07-13 21:58:52 -0500

    updated the variables in backed.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "lwm-terraform-bucket-1"
     key    = "environments/dev/terraform.tfstate"
-    region = us-east-1
+    region = "us-east-1"
     
   }
 }
